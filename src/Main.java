74223b499b26bfd930de7e85a47c1b96e42ae754
marlav <albertbanua149mv@gmail.com>
2025-11-01 10:00:28 +0200

    Add methodHello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Main.methodHello();
+    }
+    private static void methodHello() {
         System.out.print("Hello SpaceLab Java!");
     }
 }
